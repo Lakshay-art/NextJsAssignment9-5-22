b1f453001ffe6fcf87790c463eef1a2b35ae1f95
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2022-05-14 01:16:13 +0530

    redirections

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects(){
+     return[
+       {
+         source:'/',
+         destination:'/card-checkout',
+         permanent:true,
+       },
+     ]
+  }
 }
 
 module.exports = nextConfig
